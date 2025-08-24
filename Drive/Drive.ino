60a07e72f85e5749d0b01397c3be76d56af1db26
Gold87 <91761103+Gold872@users.noreply.github.com>
2025-08-24 15:52:52 -0400

    Bump version to 1.2

--- Drive/Drive.ino
+++ Drive/Drive.ino
@@ -10,7 +10,7 @@
 #define DATA_SEND_INTERVAL 50  // ms
 #define MOTOR_UPDATE_INTERVAL 10  // ms
 
-const Version version = {major: 1, minor: 1};
+const Version version = {major: 1, minor: 2};
 
 const int errorPin = 9;
 
