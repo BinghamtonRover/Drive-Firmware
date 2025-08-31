563bf90ee12d51f9753d6206944b7b4d2aee0662
jay947462 <jakasofu@bingamton.edu>
2025-08-31 13:38:40 -0400

    added current sensor to pinouts.h

--- Drive/pinouts.h
+++ Drive/pinouts.h
@@ -4,11 +4,13 @@
 #include "src/cameras/cameras.h"
 #include "src/voltage/voltage.h"
 #include "src/temperature/temperature.h"
+#include "src/current/current.h"
 
 Buttons buttons;     // see src/button/button.h for pinouts
 Motors motors;       // see src/motors/motors.h for CAN IDs
 Cameras cameras;     // see src/cameras/cameras for pinouts
 LedStrip led_strip;  // see src/led_strip/led_strip for pinouts
+CurrentSensor currentsensor(25);
 
 VoltageSensor voltageSensor(41);
 TemperatureSensor temperatureSensor(21);
