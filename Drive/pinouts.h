ce3873668aca1edbe567dbc6c08e299ebfd97dfd
jay947462 <jakasofu@binghamton.edu>
2025-11-05 21:07:38 -0500

    start of edits to pinouts; added #include for switch.h and a header guard

--- Drive/pinouts.h
+++ Drive/pinouts.h
@@ -1,9 +1,13 @@
+#ifndef PINOUTS_H
+#define PINOUTS_H
+
 #include "src/button/button.h"
 #include "src/motors/motors.h"
 #include "src/led_strip/led_strip.h"
 #include "src/cameras/cameras.h"
 #include "src/voltage/voltage.h"
 #include "src/temperature/temperature.h"
+#include "src/switch/switch.h"
 
 Buttons buttons;     // see src/button/button.h for pinouts
 Motors motors;       // see src/motors/motors.h for CAN IDs
@@ -12,3 +16,5 @@ LedStrip led_strip;  // see src/led_strip/led_strip for pinouts
 
 VoltageSensor voltageSensor(41);
 TemperatureSensor temperatureSensor(21);
+
+#endif
