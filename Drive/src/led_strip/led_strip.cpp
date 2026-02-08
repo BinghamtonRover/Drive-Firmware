dc9cf075f7e0d6b8020ed2abaab5174595a47678
pbrown222 <2pdbrown@gmail.com>
2026-02-08 12:15:45 -0500

    using fast led library to implement red/green/alternating blinking. unfinished and not compiled in arduino yet.

--- Drive/src/led_strip/led_strip.cpp
+++ Drive/src/led_strip/led_strip.cpp
@@ -1,56 +1,146 @@
-
 #include <Arduino.h>
 
 #include "led_strip.h"
 
 void LedStrip::setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  // pinMode(redPin, OUTPUT);
+  // pinMode(greenPin, OUTPUT);
+  // pinMode(bluePin, OUTPUT);
+  // blue();
+  FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
   blue();
 }
 
+// void LedStrip::applyColor(CRGB color) {
+//   analogWrite(redPin, color.r);
+//   analogWrite(greenPin, color.g);
+//   analogWrite(bluePin, color.b);
+// }
+
+void LedStrip::applyColor(const CRGB& color) {
+  fill_solid(leds, NUM_LEDS, color);
+  FastLED.show();
+}
+
 void LedStrip::red() {
-  redValue = HIGH;
-  greenValue = LOW;
-  blueValue = LOW;
+
+  shouldBlink = false;
+  currentMode = BlinkMode::NONE;
+  applyColor(CRGB::Red);
+
+
+  // redValue = HIGH;
+  // greenValue = LOW;
+  // blueValue = LOW;
   data.color = ProtoColor::ProtoColor_RED;
-  oldColor = ProtoColor::ProtoColor_RED;
+  // oldColor = ProtoColor::ProtoColor_RED;
 }
 
 void LedStrip::green() {
-  redValue = LOW;
-  greenValue = HIGH;
-  blueValue = LOW;
+
+  shouldBlink = false;
+  currentMode = BlinkMode::NONE;
+  applyColor(CRGB::Green);
+
+
+  // redValue = LOW;
+  // greenValue = HIGH;
+  // blueValue = LOW;
   data.color = ProtoColor::ProtoColor_GREEN;
-  oldColor = ProtoColor::ProtoColor_GREEN;
+  // oldColor = ProtoColor::ProtoColor_GREEN;
 }
 
 void LedStrip::blue() {
-  redValue = LOW;
-  greenValue = LOW;
-  blueValue = HIGH;
+
+  shouldBlink = false;
+  currentMode = BlinkMode::NONE;
+  applyColor(CRGB::Blue);
+
+  // redValue = LOW;
+  // greenValue = LOW;
+  // blueValue = HIGH;
   data.color = ProtoColor::ProtoColor_BLUE;
-  oldColor = ProtoColor::ProtoColor_BLUE;
+  // oldColor = ProtoColor::ProtoColor_BLUE;
 }
 
 void LedStrip::off() {
-  digitalWrite(redPin, LOW);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(bluePin, LOW);
+
+  shouldBlink = false;
+  currentMode = BlinkMode::NONE;
+  applyColor(CRGB::Black);
+
+  // digitalWrite(redPin, LOW);
+  // digitalWrite(greenPin, LOW);
+  // digitalWrite(bluePin, LOW);
   data.color = ProtoColor::ProtoColor_UNLIT;
 }
 
+// NEW BLINKING METHODS
+void LedStrip::blinkRed() {
+  currentMode = BlinkMode::BLINK_RED;
+  shouldBlink = true;
+}
+
+void LedStrip::blinkGreen() {
+  currentMode = BlinkMode::BLINK_GREEN;
+  shouldBlink = true;
+}
+
+void LedStrip::blinkAlternating() {
+  currentMode = BlinkMode::BLINK_ALTERNATING;
+  shouldBlink = true;
+}
+
+// void LedStrip::update() {
+//   if (shouldBlink && !isOn) {
+//     off();
+//   } else {
+//     digitalWrite(redPin, redValue);
+//     digitalWrite(greenPin, greenValue);
+//     digitalWrite(bluePin, blueValue);
+//     data.color = oldColor;
+//   }
+//   isOn = !isOn;
+// }
+
+// NEW UPDATE METHOD
 void LedStrip::update() {
-  if (shouldBlink && !isOn) {
-    off();
-  } else {
-    digitalWrite(redPin, redValue);
-    digitalWrite(greenPin, greenValue);
-    digitalWrite(bluePin, blueValue);
-    data.color = oldColor;
+  if (!shouldBlink || currentMode == BlinkMode::NONE) {
+    return;
   }
-  isOn = !isOn;
+
+  if (blinkTimer.hasExpired()) {
+    toggle = !toggle;
+    blinkTimer.reset();
+  }
+
+  switch (currentMode) {
+    case BlinkMode::BLINK_RED:
+      applyColor(toggle ? CRGB::Red : CRGB::Black);
+      data.color = ProtoColor::ProtoColor_RED;
+      break;
+
+    case BlinkMode::BLINK_GREEN:
+      applyColor(toggle ? CRGB::Green : CRGB::Black);
+      data.color = ProtoColor::ProtoColor_GREEN;
+      break;
+
+    case BlinkMode::ALTERNATING:
+      applyColor(toggle ? CRGB::Red : CRGB::Green);
+      data.color = ProtoColor::ProtoColor_UNLIT;
+      break;
+
+    // case BoolState::BoolState_OFF:
+    //   shouldBlink = false;
+    //   currentMode = BlinkMode::NONE;
+    //   applyColor(CRGB::Black);
+    //   break;
+
+    default:
+      break;
+  }
 }
 
 void LedStrip::handleCommand(DriveCommand command) {
--- Drive/src/led_strip/led_strip.h
+++ Drive/src/led_strip/led_strip.h
@@ -1,32 +1,50 @@
 #include "../drive.pb.h"
 #include "../utils/BURT_timer.h"
+#include <FastLED.h>
 
 const int blinkInterval = 250;  // ms
 
-const int redPin = 14;
-const int greenPin = 15;
-const int bluePin = 16;
+#define NUM_LEDS 30
+#define DATA_PIN 14
+#define CLOCK_PIN 15
 
+enum class BlinkMode {
+    NONE,
+    BLINK_RED,
+    BLINK_GREEN,
+    ALTERNATING
+};
+ 
 /// The RGB LED strip that goes around the rover.
 class LedStrip {
   private:
-    /// Whether to light the red LEDs.
-    bool redValue;
+    // /// Whether to light the red LEDs.
+    // bool redValue;
 
-    /// Whether to light the green LEDs.
-    bool greenValue;
+    // /// Whether to light the green LEDs.
+    // bool greenValue;
 
-    /// Whether to light the blue LEDs.
-    bool blueValue;
+    // /// Whether to light the blue LEDs.
+    // bool blueValue;
 
-    /// Whether the LED strip is currently lit during this phase of blinking.
-    ///
-    /// When #shouldBlink is true, #update will alternate this value to be on and off
-    /// and light the LED strip accordingly.
-    bool isOn;
+    // /// Whether the LED strip is currently lit during this phase of blinking.
+    // ///
+    // /// When #shouldBlink is true, #update will alternate this value to be on and off
+    // /// and light the LED strip accordingly.
+    // bool isOn;
 
-    /// What the old color of the
-    ProtoColor oldColor = ProtoColor::ProtoColor_UNLIT;
+    // /// What the old color of the
+    // ProtoColor oldColor = ProtoColor::ProtoColor_UNLIT;
+
+
+    // USING FAST LED (PATRICK BROWN)
+
+    CRGB leds[NUM_LEDS];
+
+    BurtTimer blinkTimer{blinkInterval};
+    bool toggle = false;
+    BlinkMode currentMode = BlinkMode::NONE;
+
 
   public:
     /// The current status of the LED strip.
@@ -52,6 +70,12 @@ class LedStrip {
 
     /// Whether the LED strip should be blinking.
     bool shouldBlink = false;
+
+    void blinkRed();
+    void blinkGreen();
+    void blinkAlternating();
+
+    void applyColor(const CRGB& color);
 
     /// Handles commands to change the LED strip.
     void handleCommand(DriveCommand command);
